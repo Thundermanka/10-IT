ad96df35c1cb984ec3e2184178d537398d22f9e4
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-11-04 20:05:39 +0300

    Update калькукатор228.c
    + еще 2 фу-ии деления, и не работающий модуль

--- калькукатор228.c
+++ калькукатор228.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+double module(double a) {
+    if(a > 0)
+    return a;
+    if(a < 0)
+    return -a;
+    if(a == 0)
+    return a;
+}
+int div(int a, int b, int* ErrorCode) {
+    if(b != 0)
+        return a / b;
+    if(b == 0)
+        *ErrorCode = -1;
+    return 0;
+}
+int mod(int a, int b, int* ErrorCode){
+    if(b != 0)
+        return a % b;
+    if(b == 0)
+        *ErrorCode = -1;
+        return 0;
+}
 double division(double a, double b, int* ErrorCode) {
     if(b != 0)
         return a / b;
@@ -31,11 +53,30 @@ int main(void) {
                     printf("ай ай ай нинада так\n");
                 ErrorCode = 0;
                 break;
+            case '%':
+                result = mod(a, b, &ErrorCode);
+                if(ErrorCode == 0)
+                    printf("%lg\n", result);
+                if(ErrorCode != 0)
+                    printf("ай ай ай нинада так\n");
+                ErrorCode = 0;
+                break;
+            case 'D':
+                result = div(a, b, &ErrorCode);
+                if(ErrorCode == 0)
+                    printf("%lg\n", result);
+                if(ErrorCode != 0)
+                    printf("ай ай ай нинада так\n");
+                ErrorCode = 0;
+                break;
+            case 'M':
+                result = module(a);
+                    printf("%lg\n", result);
+                break;
             case 'x' :
                 return 0;
                 break;
             
         }
     }
-    
 }
