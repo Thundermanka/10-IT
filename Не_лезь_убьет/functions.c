60bf5538c85cd0c4ebe1259c91c59d81febac9b1
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-11-05 14:44:33 +0300

    Update functions.c
    добавил функции

--- Не_лезь_убьет/functions.c
+++ Не_лезь_убьет/functions.c
@@ -1,1 +1,33 @@
-
+/* это фу-ия модуля, берет double, a, затем выводит double */
+double module(double a) { 
+    if(a > 0)
+    return a;
+    if(a < 0)
+    return -a;
+    if(a == 0)
+    return a;
+}
+/*фу-ия деления со взятием частного, берет int a, b, выводит int, ошика при b == 0, тогда меняем и выводим код ошибки*/
+int div(int a, int b, int* ErrorCode) {
+    if(b != 0)
+        return a / b;
+    if(b == 0)
+        *ErrorCode = -1;
+    return 0;
+}
+/*фу-ия деления со взятием остатка, берет int a, b, выводит int, ошика при b == 0, тогда меняем и выводим код ошибки*/
+int mod(int a, int b, int* ErrorCode){
+    if(b != 0)
+        return a % b;
+    if(b == 0)
+        *ErrorCode = -1;
+        return 0;
+}
+/*фу-ия деления берет double a, b, выводит double, ошика при b == 0, тогда меняем и выводим код ошибки*/
+double division(double a, double b, int* ErrorCode) {
+    if(b != 0)
+        return a / b;
+    if(b == 0)
+        *ErrorCode = -1;
+    return 0;
+}
