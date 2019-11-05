f81b0732e8791a97a7805f614d8e48b1b7e9fc41
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-11-05 15:08:48 +0300

    Update functions.c
    убрал деление, тк она должна быть в мэйне

--- Не_лезь_убьет/functions.c
+++ Не_лезь_убьет/functions.c
@@ -21,13 +21,4 @@ int mod(int a, int b, int* ErrorCode){
         return a % b;
     if(b == 0)
         *ErrorCode = -1;
-        return 0;
-}
-/*фу-ия деления берет double a, b, выводит double, ошика при b == 0, тогда меняем и выводим код ошибки*/
-double division(double a, double b, int* ErrorCode) {
-    if(b != 0)
-        return a / b;
-    if(b == 0)
-        *ErrorCode = -1;
-    return 0;
-}
+        return 0;}
