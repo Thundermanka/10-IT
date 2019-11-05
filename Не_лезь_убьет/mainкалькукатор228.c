2f915658ba32afd6096e592761d73f6040753f8a
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-11-05 15:09:24 +0300

    Update mainкалькукатор228.c
    добавил деление

--- Не_лезь_убьет/mainкалькукатор228.c
+++ Не_лезь_убьет/mainкалькукатор228.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include "functions.c"
+/*фу-ия деления берет double a, b, выводит double, ошика при b == 0, тогда меняем и выводим код ошибки*/
+double division(double a, double b, int* ErrorCode) {
+    if(b != 0)
+        return a / b;
+    if(b == 0)
+        *ErrorCode = -1;
+    return 0;
+}
 /*главная фу-ия, сканирует a,b, хранит в себе переменные, создает меню и дергает за ниточки */
 int main(void) {
     double a = 0, b = 0, result = 0;
