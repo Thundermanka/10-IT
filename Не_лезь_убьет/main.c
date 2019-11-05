bbdd4afdafa4f5da1d3fc79c19f102f789ab06ab
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-11-05 14:44:03 +0300

    Update main.c

--- Не_лезь_убьет/main.c
+++ Не_лезь_убьет/main.c
@@ -1,37 +1,4 @@
 #include <stdio.h>
-/* это фу-ия модуля, берет double, a, затем выводит double */
-double module(double a) { 
-    if(a > 0)
-    return a;
-    if(a < 0)
-    return -a;
-    if(a == 0)
-    return a;
-}
-/*фу-ия деления со взятием частного, берет int a, b, выводит int, ошика при b == 0, тогда меняем и выводим код ошибки*/
-int div(int a, int b, int* ErrorCode) {
-    if(b != 0)
-        return a / b;
-    if(b == 0)
-        *ErrorCode = -1;
-    return 0;
-}
-/*фу-ия деления со взятием остатка, берет int a, b, выводит int, ошика при b == 0, тогда меняем и выводим код ошибки*/
-int mod(int a, int b, int* ErrorCode){
-    if(b != 0)
-        return a % b;
-    if(b == 0)
-        *ErrorCode = -1;
-        return 0;
-}
-/*фу-ия деления берет double a, b, выводит double, ошика при b == 0, тогда меняем и выводим код ошибки*/
-double division(double a, double b, int* ErrorCode) {
-    if(b != 0)
-        return a / b;
-    if(b == 0)
-        *ErrorCode = -1;
-    return 0;
-}
 /*главная фу-ия, сканирует a, b, ну она какбэ главная и управляет строем и у нее есть крутая переменная ErrorCode, ну и она сама крутая, вопщемб все */
 int main(void) {
     double a = 0, b = 0, result = 0;
