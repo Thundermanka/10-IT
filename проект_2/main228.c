17103476fa773ba3e68be7a702cfcb9b57e873c9
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-12-04 09:57:58 +0300

    Update main228.c

--- проект_2/main228.c
+++ проект_2/main228.c
@@ -1,1 +1,11 @@
 
+// argc_argv.cpp: определяет точку входа для консольного приложения.
+ 
+#include "stdio.h"
+int main(int argc, char* argv[]) {
+        printf("The number of command line arguments: %d \n", argc);
+            for(int i = 0; i <= argc; i++) {
+                printf(argv[i]);// вывод всех строк из массива указателей на строки(нумерация в строках начинается с 0 )
+            }
+       return 0;
+} 
