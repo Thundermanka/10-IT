ec021ef4d18c3bc592c07159fb9ab8eec573452b
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-12-09 21:23:30 +0300

    Сравнение 4х чисел

---
+++ comparison.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c, d;
+    int min = 0;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    if(a <= b)
+        min = a;
+    else
+        min = b;
+    
+    if(min >= c)
+        min = c;
+    
+    if(min >= d)
+        min = d;
+    
+    printf("%d", min);
+    return min;
+}
