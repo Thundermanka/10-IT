5c9e91acb3cc50c0b6b858f07b34f5078a9bf035
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-12-10 14:02:33 +0300

    Delete main.c

--- проект_2/main.c
+++
@@ -1,8 +0,0 @@
- #include "stdio.h"
-int main(int argc, char* argv[]) {
-        printf("The number of command line arguments: %d \n", argc);
-            for(int i = 0; i <= argc; i++) {
-                printf(argv[i]);// вывод всех строк из массива указателей на строки(нумерация в строках начинается с 0 )
-            }
-       return 0;
-} 
