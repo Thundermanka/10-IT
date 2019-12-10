85ee3f095c5f4bae78c7fa5ac1363254c45e7d4a
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-12-10 14:18:26 +0300

    Update main1.c

--- проект_2/main1.c
+++ проект_2/main1.c
@@ -3,15 +3,15 @@
 #include <string.h>
 
 int main(int argc, char* argv[]){
-  printf("The number of command line arguments: %d \n", argc);
-  printf("Programm name: %s\n", argv[0]);
+    printf("The number of command line arguments: %d \n", argc);
+    printf("Programm name: %s\n", argv[0]);
 
-  for (int i=1; i<=agrc; i++){
-  if(!strcmp(argv[i], "FUNCTION")){ //если выполняется, то парсим Function
-  }
-if(!strcmp(argv[i], "SET")){ //если выполняется то парсим SET
-}
-else printf("error: неправильный аргумент")
-}
+    for (int i=1; i<=agrc; i++){
+    if(!strcmp(argv[i], "FUNCTION")){ //если выполняется, то парсим Function
+    }
+    if(!strcmp(argv[i], "SET")){ //если выполняется то парсим SET
+    }
+    else printf("error: неправильный аргумент")
+    }
 return 0;
 }
