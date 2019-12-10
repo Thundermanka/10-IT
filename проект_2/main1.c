2cbbd0efe75cbbd6beaa3cb8201bb2ebbcef7d00
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-12-10 14:16:29 +0300

    Update main1.c

--- проект_2/main1.c
+++ проект_2/main1.c
@@ -7,9 +7,9 @@ int main(int argc, char* argv[]){
   printf("Programm name: %s\n", argv[0]);
 
   for (int i=1; i<=agrc; i++){
-  if(!strcmp(argv[i], "FUNCTION")){ //парсим Function
+  if(!strcmp(argv[i], "FUNCTION")){ //если выполняется, то парсим Function
   }
-if(!strcmp(argv[i], "SET")){ //парсим SET
+if(!strcmp(argv[i], "SET")){ //если выполняется то парсим SET
 }
 else printf("error: неправильный аргумент")
 }
