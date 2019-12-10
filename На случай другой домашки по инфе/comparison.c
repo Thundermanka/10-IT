5c2fd968eeeaf26fb9fdac6e73f98d27ce5c6de7
Максим <33954782+Thundermanka@users.noreply.github.com>
2019-12-10 13:56:02 +0300

    Update comparison.c

--- На случай другой домашки по инфе/comparison.c
+++ На случай другой домашки по инфе/comparison.c
@@ -4,16 +4,16 @@ int main(void) {
     int a, b, c, d;
     int min = 0;
     scanf("%d%d%d%d", &a, &b, &c, &d);
-    if(a <= b)
-        min = a;
-    else
-        min = b;
+        if(a <= b)
+            min = a;
+        else
+            min = b;
     
-    if(min >= c)
-        min = c;
+        if(min >= c)
+            min = c;
     
-    if(min >= d)
-        min = d;
+        if(min >= d)
+            min = d;
     
     printf("%d", min);
     return 0;
